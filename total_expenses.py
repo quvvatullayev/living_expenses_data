6d5e4cc5730aa097d2da6aa9869ea23f110ed2e3
tajriba <quvvatullayev@gmail.com>
2022-07-14 15:02:23 +0300

    total_expenses.py

--- total_expenses.py
+++ total_expenses.py
@@ -1,4 +1,6 @@
+import json
 # Calculate the total expenses
+
 def total_expenses(monthly_expenses: dict) -> int:
     """
     Calculate the total expenses
@@ -7,4 +9,9 @@ def total_expenses(monthly_expenses: dict) -> int:
     Returns:
         total_expenses: total expenses
     """
-    return 
+    sum = 0
+    for i in monthly_expenses.values():
+        sum += i
+    return sum
+
+print(total_expenses(json.loads(open('data.json').read())))
